ce054243360e1cb868a717eacf66c82b296ec17c
Sudeepa Pal <sudeepa@onqanet.com>
2025-03-05 18:06:03 +0530

    Working on the layout of Dashboard

--- Emotipics/All Dashboard Views/Dashboard Views/DashboardViewController.swift
+++ Emotipics/All Dashboard Views/Dashboard Views/DashboardViewController.swift
@@ -9,19 +9,54 @@ import UIKit
 
 class DashboardViewController: UIViewController {
     
+    
+    @IBOutlet weak var rotateBtn: UIButton!
+    
+    
+    @IBOutlet weak var topView: UIView!
+    
+    
+    
+    @IBOutlet weak var welcomeBackLbl: UILabel!{
+        didSet{
+            //1.set the font family
+            //2. May be need make two separate labels or '+' the existing name
+            
+        }
+    }
+    
+    
+    //The Oval Card
+    
+    @IBOutlet weak var cardView: UIView!{
+        didSet{
+            cardView.layer.cornerRadius = 25
+            cardView.clipsToBounds = true
+        }
+    }
+    
+    
+    @IBOutlet weak var CatalogueLbl: UILabel!{
+        didSet{
+            //1. set the font family for the label
+            
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemCyan
-        
+        view.backgroundColor = .white
+        rotateBtn.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
         // Setup Tab Bar Controller
         setupTabBarController()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.isHidden = false
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     
