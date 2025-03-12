952ce5f393da1df6977b5f6de0f398ed103fc42a
Sudeepa Pal <sudeepa@onqanet.com>
2025-03-12 17:46:23 +0530

    Added All Catalogue View

--- Emotipics/All Dashboard Views/Dashboard Views/Catalogues/SubViews/AllCataloguesViewController.swift
+++ Emotipics/All Dashboard Views/Dashboard Views/Catalogues/SubViews/AllCataloguesViewController.swift
@@ -8,15 +8,83 @@
 import UIKit
 
 class AllCataloguesViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backGroundView: UIView! {
+        didSet{
+            backGroundView.layer.cornerRadius = 25
+            backGroundView.clipsToBounds = true
+        }
+    }
+    
+    
+    @IBOutlet weak var catalogueCollView: UICollectionView!
+    
+    
+    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    
 
+    
+    @IBOutlet weak var myCatalogueLbl: UILabel!
+    
+    @IBOutlet weak var sortByLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .white
+        catalogueCollView.dataSource = self
+        catalogueCollView.delegate = self
+        catalogueCollView.register(UINib(nibName: "EntryCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        
+        
+        myCatalogueLbl.font = UIFont(name: textInputStyle.latoBold.rawValue, size: 17)
+        sortByLbl.font = UIFont(name: textInputStyle.latoRegular.rawValue, size: 15)
+        
     }
-
-
-   
-
 }
+
+
+extension AllCataloguesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 35
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! EntryCollectionViewCell
+    
+            cell.layer.cornerRadius = 15
+            cell.clipsToBounds = true
+            return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+            return CGSize(width: 180, height: 140)
+       
+        
+        //return CGSize(width: 180, height: 130)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        /* return UIEdgeInsets(top: 10, left: 0, bottom: 5, right: 5)*/ // Leading space for the first cell
+        
+        let totalCellWidth = 180 * 2 // Two cells per row, each 180 wide
+        let totalSpacingWidth = 10 * 1 // One space between two cells
+        let horizontalInset = (collectionView.frame.width - CGFloat(totalCellWidth + totalSpacingWidth)) / 2
+        return UIEdgeInsets(top: 10, left: horizontalInset, bottom: 10, right: horizontalInset)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10 // Adjust this value to decrease or increase spacing
+    }
+    
+    
+}
+
